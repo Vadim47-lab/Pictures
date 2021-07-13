fd978301c0ddaf7555e43f9602428e32f9a2af5f
Vadim Stepanov <vadstep777@mail.ru>
2021-07-13 14:21:40 +0700

    Pictures

---
+++ Pictures/Pictures/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Pictures
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int pictures = 52;
+            int riad = 3;
+            int kolriad = 0;
+
+            for (kolriad = 0; riad < 52; kolriad++)
+            {
+                riad = riad + 3;
+            }
+            if (pictures < riad) riad = riad - pictures;
+
+            Console.WriteLine("Количество полностью заполненных рядов = " + kolriad);
+            Console.WriteLine("Количество картинок сверх меры = " + riad);
+        }
+    }
+}
